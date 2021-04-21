228b60540c9a4293ececc7da5802cb6f909f729d
Leah Mamaril <lmamaril@gmail.com>
2021-04-20 21:12:24 -0700

    delete fe folders

--- pnnl-frontend-react/src/GetPredictionLabel.js
+++
@@ -1,9 +0,0 @@
-export default function GetPredictionLabel(data) {
-    let labels = {};
-    for (const date in data) {
-        for (const label in data[date]) {
-            labels[label] = true;
-        }
-    }
-    return Object.keys(labels);
-}
--- pnnl-frontend-react/src/config.js
+++
@@ -1,3 +0,0 @@
-const PREDICTIONS_API_URL = 'http://100.26.159.108:5000';
-
-export { PREDICTIONS_API_URL };
